81307f6180223c450d3309ca590bf7f2a6eca2fc
Maxim Butrik <maximbutrik@MacBook-Air-Maxim.local>
2025-12-24 20:18:14 +0200

    Feature: PWA Support (Manifest, SW, Icon)

---
+++ sw.js
@@ -0,0 +1,53 @@
+const CACHE_NAME = 'iqosfree-v1';
+const ASSETS_TO_CACHE = [
+  './',
+  './index.html',
+  './style.css',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png',
+  './js/app.js',
+  './js/utils.js',
+  './js/charts.js',
+  './js/firebase-config.js',
+  'https://cdn.tailwindcss.com',
+  'https://cdn.jsdelivr.net/npm/chart.js'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      console.log('[Service Worker] Caching all assets');
+      return cache.addAll(ASSETS_TO_CACHE);
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  // Simple cache-first strategy
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      // Return cached response if found
+      if (response) {
+        return response;
+      }
+      // Otherwise fetch from network
+      return fetch(event.request);
+    })
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  // Clean up old caches
+  event.waitUntil(
+      caches.keys().then((cacheNames) => {
+          return Promise.all(
+              cacheNames.map((cacheName) => {
+                  if (cacheName !== CACHE_NAME) {
+                      return caches.delete(cacheName);
+                  }
+              })
+          );
+      })
+  );
+});
